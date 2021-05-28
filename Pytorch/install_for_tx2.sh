0f8e31ce83a3b8d95b15fb8f059c683b87aa02df
rduan036 <rduan036@gmail.com>
2021-05-28 15:49:54 +0800

    install for tx2 update

--- Pytorch/install_for_tx2.sh
+++ Pytorch/install_for_tx2.sh
@@ -1,5 +1,7 @@
 # install pytorch and torchvision, reference: https://forums.developer.nvidia.com/t/pytorch-for-jetson-version-1-8-0-now-available/72048
 
+cd ~\
+
 wget https://nvidia.box.com/shared/static/p57jwntv436lfrd78inwl7iml6p13fzh.whl -O torch-1.8.0-cp36-cp36m-linux_aarch64.whl
 
 sudo apt-get install python3-pip libopenblas-base libopenmpi-dev 
